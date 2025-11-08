01ba64ac84c8911824cb5d8ef21be6e16ef210c7
Tanmay Dureja <tanmaydureja@gmail.com>
2025-11-08 12:40:24 -0800

    Initial Commit

---
+++ cashback-maximizer/cashback_maximizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  cashback_maximizerApp.swift
+//  cashback-maximizer
+//
+//  Created by Tanmay Dureja on 11/8/25.
+//
+
+import SwiftUI
+
+@main
+struct cashback_maximizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
